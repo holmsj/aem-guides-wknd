9f9f6308e60b4ffb2098556234be114fd87069d8
Jeff Holmquist <jholmqui@adobe.com>
2023-03-18 07:45:51 -0700

    Move JS includes to clientlib-site

--- ui.frontend/src/libraries/site/main.ts
+++ ui.frontend/src/libraries/site/main.ts
@@ -5,4 +5,11 @@ import './main.scss';
 import './util.js';
 import './scroll-indicator';
 
+import '../components/components/languagenavigation/languagenavigation.js';
+import '../components/components/layout-container/modal.js';
+import '../components/components/navigation/navigation.js';
+import '../components/components/form/sign-in-buttons/sign-in-buttons.js';
+import '../components/components/form/sign-in-form/sign-in-form.js';
+import '../components/components/contentfragment/js/contributor.js';
 
+// ui.frontend/src/libraries/components/components/layout-container/modal.js
